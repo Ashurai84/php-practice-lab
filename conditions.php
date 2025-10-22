8143535fcc90ae4c2dd9e0ddc3e938b229446daa
Ashutosh Rai <ashutoshrai@Ashutoshs-MacBook-Air.local>
2025-10-22 20:22:46 +0530

    here we are learing about how conditions works in php

--- conditions.php
+++ conditions.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 $age= 17;
 if ($age>=20) {
     echo "hello u can drive ";
@@ -7,4 +7,27 @@ if ($age>=20) {
 else{
     echo "u cannot drive it ";
 }
+?>  
+
+<!-- Using elseif for Multiple Conditions -->
+ <?php
+
+ $marks = 60;
+ if ($marks >=90 ) {
+    echo "you are in grade A++ ";
+} elseif ($marks >=80){
+    echo " you are in Grade A";   
+}elseif($marks >=70){
+    echo " you are poor in maths ";
+} elseif($marks >=60){
+    echo " you are not eligble for scholarships";
+
+}
+else
+    echo " you are fail ";
+
 ?>
+
+
+
+

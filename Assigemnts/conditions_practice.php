a76ac3857ff7337f9d2ff290be8e138596c56f89
Ashutosh Rai <ashutoshrai@Ashutoshs-MacBook-Air.local>
2025-10-23 23:53:31 +0530

    Assiments in conditions

---
+++ Assigemnts/conditions_practice.php
@@ -0,0 +1,14 @@
+<?php 
+$age =20;
+$has_job = true;
+
+if ($age<18 && $has_job) {
+    echo "you are young amd focus on your study";
+} 
+elseif($age >= 18 && $age < 25){
+    echo "You are young and energetic!";
+
+}
+else
+    echo "Wisdom comes with age";
+?>

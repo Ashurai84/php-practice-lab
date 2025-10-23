4c843d9d1a9840bfee5031d7919d2680c2471118
Ashutosh Rai <ashutoshrai@Ashutoshs-MacBook-Air.local>
2025-10-24 00:11:00 +0530

    Assiments in conditions eith some questions

--- Assigemnts/conditions_practice.php
+++ Assigemnts/conditions_practice.php
@@ -16,24 +16,21 @@ else
 
 <!-- Write a PHP program that checks a person’s age and marks, then prints messages based on both. -->
 
- <?php
- $Age =20;
- $Marks =10;
+ <<?php
+$Age = 25;
+$Marks = 10;
 
- if ($age>18) {
-    echo " you are to young to apply";
-} elseif ($Age>=18 && $Age<=25){
-    if ($Marks>=85) {
-
+if ($Age < 18) {
+    echo "You are too young to apply.";
+} elseif ($Age >= 18 && $Age <= 25) {
+    if ($Marks >= 85) {
         echo "Excellent student and eligible!";
-    } elseif ($Age>=18 && $Age<=25){
-        if ($Marks >=75) {
-            echo "hello you are good boy ";
-        }
-    } else
-    echo" you are an average student";
-}
-else {
-    echo " you are the over limit";
+    } elseif ($Marks >= 75) {
+        echo "Hello, you are a good student.";
+    } else {
+        echo "You are an average student.";
+    }
+} else {
+    echo "You are over the age limit.";
 }
 ?>
